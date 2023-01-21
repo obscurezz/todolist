19fb6f9d0db7570e2cca8562e29b693e8ab71859
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2023-01-21 17:13:42 +0300

    created goal model

--- skypro/goals/models.py
+++ skypro/goals/models.py
@@ -4,16 +4,51 @@ from django.utils import timezone
 from core.models import User
 
 
-class GoalCategory(models.Model):
+class BaseModelMixin(models.Model):
+    class Meta:
+        abstract = True
+
     objects = models.Manager()
 
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=timezone.now())
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=timezone.now())
+
+
+class GoalCategory(BaseModelMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
-    id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=timezone.now())
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=timezone.now())
+
+
+class Status(models.IntegerChoices):
+    to_do = 1, "К выполнению"
+    in_progress = 2, "В процессе"
+    done = 3, "Выполнено"
+    archived = 4, "Архив"
+
+
+class Priority(models.IntegerChoices):
+    low = 1, "Низкий"
+    medium = 2, "Средний"
+    high = 3, "Высокий"
+    critical = 4, "Критический"
+
+
+class Goal(BaseModelMixin):
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    description = models.CharField(verbose_name="Описание", max_length=2000)
+    category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    status = models.PositiveSmallIntegerField(verbose_name="Статус", choices=Status.choices, default=Status.to_do)
+    priority = models.PositiveSmallIntegerField(verbose_name="Приоритет", choices=Priority.choices,
+                                                default=Priority.medium)
+    deadline = models.DateField(verbose_name="Дата дедлайна")
--- skypro/goals/serializers.py
+++ skypro/goals/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
 from core.serializers import ProfileSerializer
-from goals.models import GoalCategory
+from goals.models import Goal, GoalCategory
 
 
-class GoalCreateSerializer(serializers.ModelSerializer):
+class GoalCategoryCreateSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
@@ -20,3 +20,21 @@ class GoalCategorySerializer(serializers.ModelSerializer):
         model = GoalCategory
         fields = "__all__"
         read_only_fields = ("id", "created", "updated", "user")
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+    def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
