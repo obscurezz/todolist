9c7fcf1aba6378614ece5ace286f8064f1cc0494
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2023-02-05 18:00:21 +0300

    entrypoint with check isn't working, trying simple script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-python skypro/manage.py migrate --check
-status=$?
-if [[ $status != 0 ]]; then
+#python skypro/manage.py migrate --check
+#status=$?
+#if [[ $status != 0 ]]; then
+#python skypro/manage.py migrate
+#fi
+#exec "$@"
 python skypro/manage.py migrate
-fi
-exec "$@"
